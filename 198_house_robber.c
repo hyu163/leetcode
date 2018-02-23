8ddf1e7871c170b6dd8ac7fb39c3e029c1dfb1a1
Hong Yu <hyu163@yeah.net>
2018-02-23 23:13:00 +0800

    init 198 solution

---
+++ 198_house_robber.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int rob(int* nums, int numsSize){
+    return 0;
+}
+
+int main(int argc, char * argv[]){
+    int a[]={0,1,2,3,4,9,10,20,0,20};
+    int result;
+    result = rob(a, 10);
+    printf("%d\n", result);
+    return 0;
+}
